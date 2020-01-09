974a60dfe4d4596d161c0885696ec8a145cdce10
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2020-01-09 15:31:25 -0300

    commands: Deleting robotID from Command type

--- commands/command.go
+++ commands/command.go
@@ -2,14 +2,8 @@ package commands
 
 // Command - command to send
 type Command struct {
-	robotID         int
 	linearVelocity  float64
 	angularVelocity float64
-}
-
-// GetRobotID returns the robot identification
-func (command Command) GetRobotID() int {
-	return command.robotID
 }
 
 // GetLinearVelocity returns the robot linear velocity
@@ -20,11 +14,6 @@ func (command Command) GetLinearVelocity() float64 {
 // GetAngularVelocity returns the robot angular velocity
 func (command Command) GetAngularVelocity() float64 {
 	return command.angularVelocity
-}
-
-// SetRobotID sets the robot identification to the ID passed as argument
-func (command *Command) SetRobotID(id int) {
-	command.robotID = id
 }
 
 // SetLinearVelocity sets the robot linear velocity to the velocity passed as argument
