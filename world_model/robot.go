b059f4e719aef6f8129258df95dff36f12f58c16
Laura <laura.aguiar.dalmolin@gmail.com>
2020-01-21 23:33:22 -0300

    robot.go: adding comments for clarity and correcting bugs

--- world_model/robot.go
+++ world_model/robot.go
@@ -1,42 +1,50 @@
 package world_model
 
-// import
-
 type Robot struct {
-	id             int
-	role           string
-	actualPosition Pose
-	targetPosition Pose
+	id             int    // robot's id
+	role           string // robot's role
+	actualPosition Pose   // robot's actual position
+	targetPosition Pose   // robot's desired position
 }
 
+// GetID returns the robot's id
 func (robot Robot) GetID() int {
 	return robot.id
 }
 
+// GetRole returns the robot's role
 func (robot Robot) GetRole() string {
 	return robot.role
 }
 
-func (robot Robot) GetActualPosition() Pose {
+// GetActualPosition returns the robot's actual position
+func (robot Robot) GetActualPosition() float64 {
 	return robot.actualPosition.GetOrientation()
 }
 
-func (robot Robot) GetTargetPosition() Pose {
+// GetTargetPosition return the robot's target position
+func (robot Robot) GetTargetPosition() float64 {
 	return robot.targetPosition.GetOrientation()
 }
 
+// SetID sets the robot's id to the value passed as an argument
 func (robot Robot) SetID(id int) {
 	robot.id = id
 }
 
+// SetRole sets the robot's role to the value passed as an argument
 func (robot Robot) SetRole(role string) {
-	position.role = role
+	robot.role = role
 }
 
-func (robot Robot) SetActualPosition(actualPosition Pose) Pose {
-	position.actualPosition.SetActualPosition(actualPosition)
+// SetActualPosition sets the robot's actualPosition's orientation to
+// the float64 value passed as an argument
+func (robot Robot) SetActualPosition(actualPosition float64) Pose {
+	robot.actualPosition.SetOrientation(actualPosition)
 }
 
-func (robot Robot) SetTargetPosition(targetPosition Pose) Pose {
-	position.targetPosition.SetTargetPosition(targetPosition)
+// SetTargetPosition sets the robot's targetPosition's orientation to
+// the float64 value passed as an argument
+func (robot Robot) SetTargetPosition(targetPosition float64) Pose {
+	robot.targetPosition.SetOrientation(targetPosition)
 }
